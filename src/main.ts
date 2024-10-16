672d13c9de3d7ea4f4b707e7c8a4a9a422e48588
MerlinAlex <merlinalex@qq.com>
2024-10-16 17:23:49 +0800

    chore: export pinia

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,15 @@
-import { createPinia } from 'pinia'
 import { createApp } from 'vue'
 
 import App from './App.vue'
 import router from './router'
+import store from './stores'
 
 import '@unocss/reset/tailwind.css'
 import 'uno.css'
 
 const app = createApp(App)
 
-app.use(createPinia())
+app.use(store)
 app.use(router)
 
 app.mount('#app')
