d234996fd6a8e5a15d4b13270a0004bca083e430
MerlinAlex <merlinalex@qq.com>
2024-10-16 10:08:39 +0800

    chore: change server port

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,9 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    port: 3939
+  },
   plugins: [
     vue(),
     vueJsx(),
