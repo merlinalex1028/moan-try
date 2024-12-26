68259a247cd7044ec2ee1a33953f2cddef08147e
merlinalex1028 <merlinalex@qq.com>
2024-12-26 16:17:42 +0800

    feat: css

--- src/views/CustomEvent/index.vue
+++ src/views/CustomEvent/index.vue
@@ -1,13 +1,22 @@
 <script setup lang="ts">
+import { onMounted, ref } from 'vue'
 import Description from './description.md'
 import Dispatch from './dispatch.vue'
 import Listener from './listener.vue'
+
+const isShowExample = ref(false)
+
+onMounted(() => {
+  setTimeout(() => {
+    isShowExample.value = true
+  }, 1000)
+})
 </script>
 
 <template>
   <Description />
-  <div class="slide-enter">
-    <div class="flex gap-10">
+  <div v-if="isShowExample" class="slide-enter mt-4">
+    <div class="flex gap-2">
       <Dispatch class="w-full border-base rounded p-4" />
       <Listener class="w-full border-base rounded p-4" />
     </div>
