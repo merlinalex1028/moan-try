f207a7df53e46cfa3a39cc9cebfa66a6f3af6917
merlinalex1028 <merlinalex@qq.com>
2025-01-03 15:51:36 +0800

    feat: tag component

---
+++ src/components/MaTag/index.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import type { ITagProps } from '.'
+
+defineProps<ITagProps>()
+</script>
+
+<template>
+  <div class="w-fit border border-primary-400 rounded-md bg-primary-100 object-contain p-2 text-gray-6">
+    {{ text }}
+  </div>
+</template>
