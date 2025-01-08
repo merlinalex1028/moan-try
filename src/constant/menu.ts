be720f94a33578925e97318914a7c9dbec1d4dfe
merlinalex1028 <merlinalex@qq.com>
2025-01-08 18:21:43 +0800

    chore: sidebar menu performance

---
+++ src/constant/menu.ts
@@ -0,0 +1,65 @@
+export const menuList = [
+  {
+    label: 'Home',
+    items: [
+      {
+        label: 'Dashboard',
+        icon: 'pi pi-fw pi-home',
+        to: '/backstage/dashboard'
+      }
+    ]
+  },
+  {
+    label: 'Display Content',
+    items: [
+      {
+        label: 'Emoji',
+        icon: 'pi pi-fw pi-image',
+        to: '/display-content/emoji'
+      }
+    ]
+  },
+  {
+    label: 'WEB API',
+    items: [
+      {
+        label: 'CustomEvent',
+        icon: 'pi pi-fw pi-send',
+        to: '/webapi/custom'
+      }
+    ]
+  },
+  {
+    label: 'Programming Techniques',
+    items: [
+      {
+        label: 'BetterCode',
+        icon: 'pi pi-fw pi-code',
+        items: [
+          {
+            label: 'collection',
+            to: '/programming-techniques/better-code/index'
+          },
+          {
+            label: 'async/await',
+            to: '/programming-techniques/better-code/async-await'
+          },
+          {
+            label: 'error handling',
+            to: '/programming-techniques/better-code/error-handling'
+          }
+        ]
+      }
+    ]
+  },
+  {
+    label: 'Build Tool',
+    items: [
+      {
+        label: 'Vite Basic',
+        icon: 'i-tabler-brand-vite',
+        to: '/build-tool/vite-basic'
+      }
+    ]
+  }
+]
--- src/views/layout/SideBar/Menu.vue
+++ src/views/layout/SideBar/Menu.vue
@@ -1,63 +1,10 @@
 <script setup lang="ts">
 import type { IMenuItem } from '@/types/sidebar'
+import { menuList } from '@/constant/menu'
 import { ref } from 'vue'
 import MenuItem from './MenuItem.vue'
 
-const model = ref<IMenuItem[]>([
-  {
-    label: 'Home',
-    items: [
-      {
-        label: 'Dashboard',
-        icon: 'pi pi-fw pi-home',
-        to: '/backstage/dashboard'
-      }
-    ]
-  },
-  {
-    label: 'Display Content',
-    items: [
-      {
-        label: 'Emoji',
-        icon: 'pi pi-fw pi-image',
-        to: '/display-content/emoji'
-      }
-    ]
-  },
-  {
-    label: 'WEB API',
-    items: [
-      {
-        label: 'CustomEvent',
-        icon: 'pi pi-fw pi-send',
-        to: '/webapi/custom'
-      }
-    ]
-  },
-  {
-    label: 'Programming Techniques',
-    items: [
-      {
-        label: 'BetterCode',
-        icon: 'pi pi-fw pi-code',
-        items: [
-          {
-            label: 'collection',
-            to: '/programming-techniques/better-code/index'
-          },
-          {
-            label: 'async/await',
-            to: '/programming-techniques/better-code/async-await'
-          },
-          {
-            label: 'error handling',
-            to: '/programming-techniques/better-code/error-handling'
-          }
-        ]
-      }
-    ]
-  }
-])
+const model = ref<IMenuItem[]>(menuList)
 </script>
 
 <template>
